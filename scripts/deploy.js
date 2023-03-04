fbe1585e14dd7a1a74cf4cab2b687a52587e39bb
oemerfurkan <omerfurkanyuruk1@gmail.com>
2023-03-04 18:30:13 +0300

    setup

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,27 +5,42 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const path = require("path");
+const fs = require("fs");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  // ethers is available in the global scope
+  const [deployer] = await ethers.getSigners();
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
   );
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(deployer.address);
+  await lock.deployed();
+  saveFrontendFiles(lock);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// we add this part to save artifacts and address
+function saveFrontendFiles(lock) {
+  const contractsDir = path.join(__dirname, "/../frontend/src/contracts");
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+  fs.writeFileSync(
+    contractsDir + "/contract-address.json",
+    JSON.stringify({ Lock: lock.address }, null, 2)
+  );
+  // `artifacts` is a helper property provided by Hardhat to read artifacts
+  const CMArtifact = artifacts.readArtifactSync("Lock");
+  fs.writeFileSync(
+    contractsDir + "/Lock.json",
+    JSON.stringify(CMArtifact, null, 2)
+  );
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

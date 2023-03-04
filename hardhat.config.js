41a3aa72ede0e3bf006b89f35c28a11f11b57716
oemerfurkan <omerfurkanyuruk1@gmail.com>
2023-03-04 21:46:20 +0300

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    // the network name
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`, // the node URL
-      accounts: [GOERLI_PRIVATE_KEY], // the accounts accessible via `ethers`
-    },
-  },
 };
